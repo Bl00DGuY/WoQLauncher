ca575f7725d07e8cc9fbc5903c71ec42a044720e
Bl00DGuY <themike0785@gmail.com>
2014-05-22 16:14:56 -0400

    First Commit. Base functions done.

---
+++ WoQLauncher/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WoQLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Prepare the process to run
+            ProcessStartInfo start = new ProcessStartInfo();
+            // Enter in the command line arguments, everything you would enter after the executable name itself
+            start.Arguments = null;
+            // Enter the executable to run, including the complete path
+            start.FileName = "WoQUpdater.EXE";
+            // Do you want to show a console window?
+            start.WindowStyle = ProcessWindowStyle.Normal;
+            start.CreateNoWindow = false;
+
+            Process proc = Process.Start(start);
+
+            Application.Exit();
+        }
+    }
+}
