56fde6779b6894361df4dc86d92fa8f6ca286400
Bl00DGuY <themike0785@gmail.com>
2014-05-23 11:39:35 -0400

    First public release

--- WoQLauncher/Form1.Designer.cs
+++ WoQLauncher/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -45,35 +48,84 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 467);
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(6, 464);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 36);
+            this.button1.Size = new System.Drawing.Size(137, 45);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Jouer";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(524, 467);
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(149, 464);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 36);
+            this.button2.Size = new System.Drawing.Size(137, 45);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Mettre à jour";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(568, 487);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "$Version";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(568, 471);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Version :";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(292, 464);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 45);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(649, 512);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "W o r l d  O f  Q u e b e c";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +134,9 @@
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WoQLauncher/Form1.cs
+++ WoQLauncher/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -27,12 +29,67 @@ namespace WoQLauncher
             // Enter the executable to run, including the complete path
             start.FileName = "WoQUpdater.EXE";
             // Do you want to show a console window?
-            start.WindowStyle = ProcessWindowStyle.Normal;
+            start.WindowStyle = ProcessWindowStyle.Hidden;
             start.CreateNoWindow = false;
 
             Process proc = Process.Start(start);
 
             Application.Exit();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Prepare the process to run
+            ProcessStartInfo start = new ProcessStartInfo();
+            // Enter in the command line arguments, everything you would enter after the executable name itself
+            start.Arguments = null;
+            // Enter the executable to run, including the complete path
+            start.FileName = "wow.EXE";
+            // Do you want to show a console window?
+            start.WindowStyle = ProcessWindowStyle.Hidden;
+            start.CreateNoWindow = false;
+
+            Process proc = Process.Start(start);
+
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetVersion();
+        }
+
+        public void GetVersion()
+        {
+            string text = System.IO.File.ReadAllText(@"Version.woq");
+
+            var client = new WebClient();
+            string result = client.DownloadString("http://dev.woq.ca/_Zip/Remote.woq");
+            label1.Text = text;
+
+            if (text != result)
+            {
+                new WebClient().DownloadFile("http://dev.woq.ca/_Zip/Remote.woq", "Version.woq");
+                // Prepare the process to run
+                ProcessStartInfo start = new ProcessStartInfo();
+                // Enter in the command line arguments, everything you would enter after the executable name itself
+                start.Arguments = null;
+                // Enter the executable to run, including the complete path
+                start.FileName = "WoQUpdater.EXE";
+                // Do you want to show a console window?
+                start.WindowStyle = ProcessWindowStyle.Hidden;
+                start.CreateNoWindow = false;
+
+                Process proc = Process.Start(start);
+
+                Application.Exit();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Fonction à venir dans une version future du lanceur.");
+        }
+
     }
 }
